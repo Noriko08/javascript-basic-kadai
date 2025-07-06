8e4741f3144711c53c375f5f4aac2b2dbc438aee
Noriko08 <152842651+Noriko08@users.noreply.github.com>
2025-07-06 10:48:56 +0900

    課題アップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+//プログラム実行日を出力
+const today = new Date();
+console.log(today);
+const [year, month, day] = [
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate(),
+];
+
+const date = year + '年' + month + '月' + day + '日';
+console.log(date);
